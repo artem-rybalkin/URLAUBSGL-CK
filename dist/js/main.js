aab0866bac83e354da9b2c46728a408f383d9549
artem-rybalkin <artem.rybalkin86@gmail.com>
2016-10-25 16:23:55 +0300

    change url

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,7 +13,7 @@ var zapros ;
 
 function request( zapros ){
     
-  var url = 'http://api.pixplorer.co.uk/image?word='+zapros+'&amount=7&size=tb';
+  var url = 'https://pixabay.com/api/?key=3168519-28357a1b07f864f5473734c78&q='+value;
 //for IE8
     if (window.XMLHttpRequest) {
         xhr = new XMLHttpRequest();
